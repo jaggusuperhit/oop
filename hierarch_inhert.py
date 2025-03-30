b36ff5fcec38ad0a7201f4533f18628a9d43ca87
jaggusuperhit <jagtapsuraj636@gmail.com>
2025-03-30 20:13:09 +0530

    tyeps of inheritance

--- hierarch_inhert.py
+++ hierarch_inhert.py
@@ -2,19 +2,22 @@
 class Parent:
     def __init__(self, name):
         self.name = name
-        
+
     def greet(self):
         print(f"Hello, my name is {self.name}.")
+
 
 # Derived class 1
 class Child1(Parent):
     def play(self):
         print(f"{self.name} is playing.")
 
+
 # Derived class 2
 class Child2(Parent):
     def study(self):
         print(f"{self.name} is studying")
+
 
 # Create instances of Child1 and Child2
 child1 = Child1("Dave")
