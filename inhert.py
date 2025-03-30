0188f1996cb8eb9dfb571c6af2f65f90fdb3e701
jaggusuperhit <jagtapsuraj636@gmail.com>
2025-03-30 18:09:25 +0530

    basics of inheritance

--- inhert.py
+++ inhert.py
@@ -1,24 +1,28 @@
 # Simple inheritance
 
+
 # Base class
 class Animal:
     def __init__(self, name):
         self.name = name
+
     def speak(self):
-        print (f"{self.name} makes a sound .")
+        print(f"{self.name} makes a sound .")
+
 
 # Derived class
 class Dog(Animal):
     # when you use constructor in child class then it could not call the parent constructor attributes its called constructor overloading.
     # def __init__(self):
     #     self.behaviour = "friendly"
-    # It can use parent class method 
+    # It can use parent class method
     # def speak(self):
 
-    # Also create new method 
-    # Method overloading 
+    # Also create new method
+    # Method overloading
     def speak1(self):
-        print(f" {self.name} barks.")  
+        print(f" {self.name} barks.")
+
 
 # create an instance of Animal
 # animal = Animal("Genric Animal")
--- super.py
+++ super.py
@@ -6,18 +6,18 @@ class Animal:
     def speak(self):
         print(f"{self.name} makes a sound")
 
+
 # Derived class
 class Dog(Animal):
-    def __init__(self,breed):
+    def __init__(self, breed):
         super().__init__()
         self.breed = breed
 
     def speak(self):
-        super().speak() # call the base class method. Super can only use through child class. 
+        super().speak()  # call the base class method. Super can only use through child class.
         print(f"{self.name} barks.It is a {self.breed}")
-    
+
+
 # create an instance of Dog
 dog = Dog("Golden Retreiver")
 dog.speak()
-
-
